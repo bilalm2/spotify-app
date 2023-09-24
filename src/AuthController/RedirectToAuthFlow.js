fd05bcee99030a8658716c6ed7bd06b55772b5c9
Bilal Mahmood <bilal.mahmood1706@gmail.com>
2023-09-24 14:54:27 -0400

    update

--- src/AuthController/RedirectToAuthFlow.js
+++ src/AuthController/RedirectToAuthFlow.js
@@ -2,13 +2,13 @@ export async function redirectToAuthCodeFlow(clientId) {
     const verifier = generateCodeVerifier(128);
     const challenge = await generateCodeChallenge(verifier);
 
-        localStorage.setItem("verifier", verifier);
+    localStorage.setItem("verifier", verifier);
 
 
     const params = new URLSearchParams();
     params.append("client_id", clientId);
     params.append("response_type", "code");
-    params.append("redirect_uri", "http://localhost:8888/callback");
+    params.append("redirect_uri", "http://localhost:5173/callback");
     params.append("scope", "user-read-private user-read-email");
     params.append("code_challenge_method", "S256");
     params.append("code_challenge", challenge);
@@ -27,10 +27,10 @@ function generateCodeVerifier(length) {
 }
 
 async function generateCodeChallenge(codeVerifier) {
-        const data = new TextEncoder().encode(codeVerifier);
-        const digest = await window.crypto.subtle.digest('SHA-256', data);
-        return btoa(String.fromCharCode.apply(null, [...new Uint8Array(digest)]))
-            .replace(/\+/g, '-')
-            .replace(/\//g, '_')
-            .replace(/=+$/, '');
+    const data = new TextEncoder().encode(codeVerifier);
+    const digest = await window.crypto.subtle.digest('SHA-256', data);
+    return btoa(String.fromCharCode.apply(null, [...new Uint8Array(digest)]))
+        .replace(/\+/g, '-')
+        .replace(/\//g, '_')
+        .replace(/=+$/, '');
 }
--- src/app.js
+++ src/app.js
@@ -1,22 +1,25 @@
-import { redirectToAuthCodeFlow } from './AuthController/RedirectToAuthFlow.js';
-import { getAccessToken } from './AuthController/GetAccessToken.js';
+import {redirectToAuthCodeFlow} from './AuthController/RedirectToAuthFlow.js';
+import {fetchAccessToken} from './AuthController/FetchAccessToken.js';
+import {fetchProfile} from './Profile/FetchProfile.js'
+import {populateUI} from './Artists/PopulateUI.js'
 
 
 const clientId = "f8435734ad604359be9029b20de31971";
-if(typeof window !== undefined) {
-    const params = new URLSearchParams(window.location.search);
-    const code = params.get("code");
-}
-else {
-    console.log("error")
+const params = new URLSearchParams(window.location.search);
+const code = params.get("code");
+const accessToken = sessionStorage.getItem("access_token");
+
+if (!accessToken) {
+    if (!code) {
+        await redirectToAuthCodeFlow(clientId);
+    } else {
+        const accessToken = await fetchAccessToken(clientId, code);
+        sessionStorage.setItem("access_token", accessToken);
+    }
 }
 
-if (!code) {
-    redirectToAuthCodeFlow(clientId);
-} else {
-    const accessToken = await getAccessToken(clientId, code);
-    console.log(accessToken);
-}
+const profile = await fetchProfile(sessionStorage.getItem("access_token"));
+populateUI(profile);
 
 
 
