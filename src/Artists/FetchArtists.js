c54f4f3423a6d6f187d3b9a704379209811b5732
Bilal Mahmood <bilal.mahmood1706@gmail.com>
2023-09-25 00:19:05 -0400

    update

--- src/Artists/FetchArtists.js
+++ src/Artists/FetchArtists.js
@@ -1,5 +1,5 @@
-export async function getArtists(token){
-    const result = await fetch("https://api.spotify.com/v1/artists", {
+export async function fetchTopArtists(token){
+    const result = await fetch("https://api.spotify.com/v1/me/top/artists", {
         method: "GET", headers: { Authorization: `Bearer ${token}`}
     });
     return await result.json();
--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 import {redirectToAuthCodeFlow} from './AuthController/RedirectToAuthFlow.js';
 import {fetchAccessToken} from './AuthController/FetchAccessToken.js';
 import {fetchProfile} from './Profile/FetchProfile.js'
-import {populateUI} from './Artists/PopulateUI.js'
+import {fetchTopArtists} from './Artists/FetchArtists.js'
+import {populateUI} from './PopulateUI.js'
 
 
 const clientId = "f8435734ad604359be9029b20de31971";
