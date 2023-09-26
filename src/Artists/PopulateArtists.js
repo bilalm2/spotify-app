e96ce2b5e1e86cdacad1c620e2edabf14c4561ca
Bilal Mahmood <bilal.mahmood1706@gmail.com>
2023-09-26 18:31:10 -0400

    update

--- src/Artists/PopulateArtists.js
+++ src/Artists/PopulateArtists.js
@@ -1,3 +1,37 @@
 export function populateArtists(artists) {
-    document.getElementById("firstArtist").innerText = artists[1].name;
+    document.getElementById("firstArtist").innerText = artists.items[0].name;
+    document.getElementById("secondArtist").innerText = artists.items[1].name;
+    document.getElementById("thirdArtist").innerText = artists.items[2].name;
+    document.getElementById("fourthArtist").innerText = artists.items[3].name;
+    document.getElementById("fifthArtist").innerText = artists.items[4].name;
+
+    const firstImage = new Image(60, 60);
+    firstImage.src = artists.items[0].images[0].url;
+    document.getElementById("firstArtistImage").appendChild(firstImage);
+
+    const secondImage = new Image(60, 60);
+    secondImage.src = artists.items[1].images[0].url;
+    document.getElementById("secondArtistImage").appendChild(secondImage);
+
+    const thirdImage = new Image(60, 60);
+    thirdImage.src = artists.items[2].images[0].url;
+    document.getElementById("thirdArtistImage").appendChild(thirdImage);
+
+    const fourthImage = new Image(60, 60);
+    fourthImage.src = artists.items[3].images[0].url;
+    document.getElementById("fourthArtistImage").appendChild(fourthImage);
+
+    const fifthImage = new Image(60, 60);
+    fifthImage.src = artists.items[4].images[0].url;
+    document.getElementById("fifthArtistImage").appendChild(fifthImage);
+
+    document.getElementById("firstUrl").setAttribute("href", artists.items[0].external_urls.spotify);
+
+    document.getElementById("secondUrl").setAttribute("href", artists.items[1].external_urls.spotify);
+
+    document.getElementById("thirdUrl").setAttribute("href", artists.items[2].external_urls.spotify);
+
+    document.getElementById("fourthUrl").setAttribute("href", artists.items[3].external_urls.spotify);
+
+    document.getElementById("fifthUrl").setAttribute("href", artists.items[4].external_urls.spotify);
 }
--- src/Profile/PopulateProfile.js
+++ src/Profile/PopulateProfile.js
@@ -1,15 +1,31 @@
 export function populateProfile(profile) {
-    document.getElementById("displayName").innerText = profile.display_name;
-    if (profile.images[0]) {
-        const profileImage = new Image(200, 200);
-        profileImage.src = profile.images[0].url;
-        document.getElementById("avatar").appendChild(profileImage);
-        document.getElementById("imgUrl").innerText = profile.images[0].url;
+    if (elementExists("displayName")) {
+        document.getElementById("displayName").innerText = profile.display_name;
     }
-    document.getElementById("id").innerText = profile.id;
-    document.getElementById("email").innerText = profile.email;
-    document.getElementById("uri").innerText = profile.uri;
-    document.getElementById("uri").setAttribute("href", profile.external_urls.spotify);
-    document.getElementById("url").innerText = profile.href;
-    document.getElementById("url").setAttribute("href", profile.href);
+    if (profile.images[0]) {
+        const profileImage = new Image(150, 150);
+        profileImage.src = profile.images[0].url;
+        if (elementExists("avatar")) {
+            document.getElementById("avatar").appendChild(profileImage);
+        }
+        if (elementExists("imgUrl")) {
+            document.getElementById("imgUrl").innerText = profile.images[0].url;
+        }
+    }
+    if (elementExists("id")) {
+        document.getElementById("id").innerText = profile.id;
+    }
+    if (elementExists("email")) {
+        document.getElementById("email").innerText = profile.email;
+    }
+    if (elementExists("uri")) {
+        document.getElementById("uri").innerText = profile.uri;
+        document.getElementById("uri").setAttribute("href", profile.external_urls.spotify);
+        document.getElementById("url").innerText = profile.href;
+        document.getElementById("url").setAttribute("href", profile.href);
+    }
+}
+
+function elementExists(id) {
+    return document.getElementById(id) !== null;
 }
