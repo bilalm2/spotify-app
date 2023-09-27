cdf1d2f18fc5da71502c5c1b96ae585024b232ff
Bilal Mahmood <bilal.mahmood1706@gmail.com>
2023-09-26 23:07:08 -0400

    update

--- src/AuthController/RedirectToAuthFlow.js
+++ src/AuthController/RedirectToAuthFlow.js
@@ -8,7 +8,7 @@ export async function redirectToAuthCodeFlow(clientId) {
     const params = new URLSearchParams();
     params.append("client_id", clientId);
     params.append("response_type", "code");
-    params.append("redirect_uri", "https://spotifystats4.netlify.app/callback");
+    params.append("redirect_uri", "https://spotifystats4.netlify.app");
     params.append("scope", "user-read-private user-read-email user-top-read");
     params.append("code_challenge_method", "S256");
     params.append("code_challenge", challenge);
